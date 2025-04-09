4deebf512a84639970993cd9de70e30344710344
NAIM <reyholder7@gmail.com>
2025-04-09 13:49:32 +0600

    Edited

--- Mammals/mammals.js
+++ Mammals/mammals.js
@@ -3,23 +3,23 @@ const sideBar = document.querySelector(".sidebar");
 const close = document.querySelector(".close");
 
 menuBar.addEventListener("click", () => {
-    showSideBar();
-  });
-  
-  const showSideBar = () => {
-    sideBar.classList.remove("show");
-    sideBar.style.display = "flex";
-    void sideBar.offsetWidth;
-    sideBar.classList.add("show");
-  };
-  
-  close.addEventListener("click", () => {
-    closeSideBar();
-  });
-  
-  const closeSideBar = () => {
-    sideBar.classList.remove("show");
-    sideBar.style.display = "none";
-    void sideBar.offsetWidth;
-    sideBar.classList.add("show");
-  };
+  showSideBar();
+});
+
+const showSideBar = () => {
+  sideBar.classList.remove("show");
+  sideBar.style.display = "flex";
+  void sideBar.offsetWidth;
+  sideBar.classList.add("show");
+};
+
+close.addEventListener("click", () => {
+  closeSideBar();
+});
+
+const closeSideBar = () => {
+  sideBar.classList.remove("show");
+  sideBar.style.display = "none";
+  void sideBar.offsetWidth;
+  sideBar.classList.add("show");
+};
